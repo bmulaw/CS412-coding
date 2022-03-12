abe85437200d314dbf0dfdd4f4d1b09b33883a3d
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-50.bu.edu>
2022-03-12 04:48:54 -0500

    completed assignment

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.post('/test', (req, res, next) => {
-  console.log(req)
-  res.render('index', { title: 'Hey there, ' + req.body.name });
+router.get('/get-customer', (req, res, next) => {
+  res.render('index', { title: 'Hi, welcome ', name: 'Customer', nameLength: 8 });
+  return {'name' : 'Customer', 'nameLength': 8}
 });
+
+router.post('/get-customer-name', (req, res, next) => {
+  res.render('index', { title: 'Hi, welcome ', name: req.body.name , nameLength: req.body.name.length });
+  return { 'name' : req.body.name, 'nameLength': req.body.name.length }
+});
+
+router.get('/name/:username', (req, res, next) => {
+  const username = req.params.username;
+  const nameLength = username.length;
+  res.render('index', { title: 'Hi, welcome ', name: username , nameLength: nameLength });
+  return { 'name' : username, 'nameLength': nameLength }
+})
 
 module.exports = router;
