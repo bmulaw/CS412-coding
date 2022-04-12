74eeacea477692bb11e73b8cb56da15eba94065b
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-50.bu.edu>
2022-04-12 17:59:49 -0400

    refactored to original template and successfully made api call on express

--- routes/ps4.js
+++ routes/ps4.js
@@ -1,9 +1,95 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const request = require('request');
 
-/* GET users listing. */
-router.post('/country', (req, res, next) => {
-  
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.body)
+  res.render('index', { title: 'Country' });
 });
+
+router.post('/capital', (req, res, next) => {
+  
+  let url = `https://restcountries.com/v3.1/name/${req.body.country}`
+  const requestOptions = {
+    url: url,
+    method: 'GET',
+    json: {},
+    qs: {
+      offset: 20
+    }
+  };
+  console.log(url)
+  
+  request(requestOptions, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let capital = body[0].capital[0];
+      res.render('index', { capital: `${req.body.country}'s capital is ${capital}`, country: req.body.country, title: 'Country'});
+    } else {
+      console.log(response.statusCode);
+    }
+  });
+});
+
+
+router.post('/population', (req, res, next) => {
+  
+  let url = `https://restcountries.com/v3.1/name/${req.body.country}`
+  const requestOptions = {
+    url: url,
+    method: 'GET',
+    json: {},
+    qs: {
+      offset: 20
+    }
+  };
+  console.log(url)
+  
+  request(requestOptions, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let population = body[0].population;
+      res.render('index', { population: `${req.body.country}'s population is ${population}`, country: req.body.country, title: 'Country'});
+    } else {
+      console.log(response.statusCode);
+    }
+  });
+});
+
+
+
+router.post('/flag', (req, res, next) => {
+  
+  let url = `https://restcountries.com/v3.1/name/${req.body.country}`
+  const requestOptions = {
+    url: url,
+    method: 'GET',
+    json: {},
+    qs: {
+      offset: 20
+    }
+  };
+  console.log(url)
+  
+  
+  request(requestOptions, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let flag = body[0].flag;
+      res.render('index', { flag: `${req.body.country}'s flag is ${flag}`, country: req.body.country, title: 'Country'});
+    } else {
+      console.log(response.statusCode);
+    }
+  });
+});
+
+
+router.use(express.urlencoded({
+  extended: true
+}))
 
 module.exports = router;
