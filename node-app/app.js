166d03fad7935b0af73448e3c1a1654156b9ab7d
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-50.bu.edu>
2022-05-07 05:06:55 -0400

    node express backend working

---
+++ node-app/app.js
@@ -0,0 +1,48 @@
+const axios = require("axios")
+const express = require('express')
+const app = express()
+
+const getCapital = (query) => {
+
+    return new Promise ((resolve, reject) => {
+        axios.request(query)
+        .then(res => {
+          let capital = res.data[0].capital[0];
+          resolve(capital);
+        })
+        .catch(error => {
+          reject(error)
+      });
+    })
+}
+
+app.get('/capital', (req, res) => {
+
+    let country = req.query.country;
+    let query = {
+        url: `https://restcountries.com/v3.1/name/${country}`
+    }
+    
+    let capital;
+    getCapital(query)
+      .then(data => capital = data)
+      .catch(err => {
+          let errorMessage = err.request.res.statusCode + " " + err.request.res.statusMessage;
+          capital = errorMessage;
+        })
+    
+    // setTimeout for 1.5 second (1500 ms) so we wait for the result of the API request above
+    setTimeout(() => {
+    //   res.send(`${country}'s capital is ${capital}`)
+      res.json({
+        "statusCode": 200,
+        "statusMessage": "Connection is good",
+        "capital": capital
+    })
+    }, 500)
+    
+  })
+
+app.listen(3000, (req, res) => {
+    console.log('running at port 3000')
+})
